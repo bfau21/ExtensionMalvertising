c2beb61cded584e4342876bca37364ffc4bc1502
bfau21 <benoit@thefau.com>
2025-01-29 21:43:33 +0100

    correction popup message

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const MICROSERVICE1_URL = "http://192.168.65.28:50001";
+const MICROSERVICE1_URL = "http://192.168.65.44:50001";
 const MICROSERVICE2_URL = "http://localhost:50002/Word2Vec";
 const MICROSERVICE3_URL = "http://localhost:50003/EasyOCR_BERT";
 const MICROSERVICE4_URL = "http://localhost:50004/predict";
@@ -14,13 +14,41 @@ async function sendToMicroservice(url, data) {
   return response.json();
 }
 
+async function sendToMicroservice2(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: data
+  });
+  return response.text();
+}
+
+async function sendToMicroservice3(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  return response.text();
+}
+
 
 async function checkAdSecurity(tabId, url) {
   try {
     console.log("IN")
     // Envoyer l'URL au microservice 1 pour récupérer les images de pubs
-    // const result_analyzer = await sendToMicroservice(MICROSERVICE1_URL, { url });
-    let result_analyzer = 1;
+    // let result_analyzer = await sendToMicroservice2(MICROSERVICE1_URL, url );
+    let result_analyzer = await sendToMicroservice2(MICROSERVICE1_URL, url );
+    console.log(result_analyzer)
+    result_analyzer = parseInt(result_analyzer);
+    console.log(result_analyzer)
+    //result_analyzer = 1;
+    // let result_analyzer = 1;
+
     // Envoyer l'URL au microservice 2 pour la convertir en vecteur
     let urlVector = await sendToMicroservice(MICROSERVICE2_URL, { url });
     urlVector = urlVector['processed_data']
@@ -40,16 +68,17 @@ async function checkAdSecurity(tabId, url) {
       m2 : urlVector,
       m3 : imageVectorDataset
     });
-    console.log("prediction = " + prediction['message'])
+    console.log("prediction = " + prediction['processed_data'])
     if (prediction['processed_data'] == "-1") {
       result_model = -1; //Malvertising detecte
     }
+
 
     if (result_model + result_analyzer <= 0) {
       showNotification("Alerte Malvertising", "Malvertising détecté : la page n'est pas sécurisée.")
     }
     else {
-        showNotification("Sécurité des publicités", "La page contient des publicités, mais elles ne sont pas sécurisées.");
+        showNotification("Sécurité des publicités", "La page contient des publicités, mais elles sont sécurisées.");
     }
   } catch (error) {
     console.error("Error checking ad security:", error);
